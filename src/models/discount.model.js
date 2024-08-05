025a0ac90aeeb7b0f71cee6b3a5f5ba79c8f0010
trinhkhaiDev <khaitv21@uef.edu.vn>
2024-08-05 20:21:06 +0700

    Create Discount Model

---
+++ src/models/discount.model.js
@@ -0,0 +1,33 @@
+'use strict'
+const e = require('express');
+// With strict mode, you can not use undeclared variables.
+
+const { model, Schema, Types } = require('mongoose');
+
+const DOCUMENT_NAME = 'Discount';
+const COLLECTION_NAME = 'Discounts';
+
+const discountSchema = new Schema({
+    discount_name: { type: String, required: true },
+    discount_description: { type: String, required: true },
+    discount_type: { type: String, default: 'fixed_amount' }, // percentage, fixed_amount
+    discount_value: { type: Number, required: true }, // 10.000 10%
+    discount_code: {type: String, required: true},
+    discount_start_date: { type: Date, required: true },
+    discount_end_date: { type: Date, required: true },
+    discount_max_use: { type: Number, required: true }, // how many times this discount can be used
+    discount_use_count: { type: Number, required: true }, // how many times this discount has been used
+    discount_users_used: { type: Array, default: [] }, // which users have used this discount
+    discount_max_uses_per_users: { type: Number, required: true }, // how many times this discount can be used by a user
+    discount_min_order_value: { type: Number, required: true }, // minimum order value to use this discount
+    discount_shop_owner: { type: Schema.Types.ObjectId, ref: 'Shop' }, // which shop this discount belongs to
+    discount_is_active: { type: Boolean, default: true },
+    discount_applies_to: {type: String, required: true, enum: ['all', 'specific_products']},
+    discount_product_id: { type: Array, default: [] }, // which products this discount applies to
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+});
+
+
+module.exports = model(DOCUMENT_NAME, discountSchema);
